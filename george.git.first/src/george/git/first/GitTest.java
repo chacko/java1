6f62e5897586d3ffadae8c2cc3539cb54b04742e
George Chacko <georgecko@gmail.com>
2014-03-05 18:31:39 -0700

    initial commit

---
+++ george.git.first/src/george/git/first/GitTest.java
@@ -0,0 +1,10 @@
+package george.git.first;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Praise The Lord!!!");
+	}
+
+}
